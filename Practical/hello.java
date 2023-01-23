775cb58ef64ddf10237601503b92dbc87f1f0512
NikitaBairi <123160958+NikitaBairi@users.noreply.github.com>
2023-01-23 11:45:26 +0530

    Update hello.java

--- Practical/hello.java
+++ Practical/hello.java
@@ -1,6 +1,11 @@
+class My{
+  public static void Display(){//static method
+  System.out.println("Hii..");
+}
+}
 class hello{
-
   public static void main(String args[]){
-       System.out.println("Its my first program.");
+  System.out.println("Its my first program.");
+  My.Display();  //classname.methodname
   }
 }
