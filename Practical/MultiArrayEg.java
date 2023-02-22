e50ee50973de941fbd6f6a161a0fff98117ec364
NikitaBairi <123160958+NikitaBairi@users.noreply.github.com>
2023-02-22 15:48:23 +0530

    Add files via upload

---
+++ Practical/MultiArrayEg.java
@@ -0,0 +1,18 @@
+package com.practical;
+
+public class MultiArrayEg {
+
+	public static void main(String[] args) {
+		int arr1[][]= {{9,8,7},{1,2,3},{4,5,5}};
+		
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				System.out.print(arr1[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
