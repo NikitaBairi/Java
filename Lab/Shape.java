2aa3461eb2a013a7104677aee5d70e4165636cd7
NikitaBairi <123160958+NikitaBairi@users.noreply.github.com>
2023-02-24 00:49:15 +0530

    Add files via upload

---
+++ Lab/Shape.java
@@ -0,0 +1,10 @@
+package com.Geometry;//Package name
+import My.Circle;//importing  from my package class circle
+
+public class Shape {
+	public static void main(String[] args) {
+		Circle c = new Circle();
+	
+	}
+
+}
